ed25021cee89d7f6ba33225abfc719891a051ae4
nameless <the.nameless.one0@gmail.com>
2012-12-14 16:20:26 +1000

    Prototype of the Biotriz system; Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biotriz::Application.config.secret_token = 'c705f9c6417452d69acaa6a4edd97971d084e97d152471ae117bd16a13a1766bcfc0e6ae2567c71ee9d05e17d44ea62cba7ba82152e1c7d1e570975f968a4e21'
