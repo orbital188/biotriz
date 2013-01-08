260aefe9377a455c6fd1c1f6c4e6174ccea05b37
nameless <the.nameless.one0@gmail.com>
2013-01-08 19:00:34 +1100

    Tuned views

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,7 +1,19 @@
+# == Schema Information
+#
+# Table name: parameters
+#
+#  id          :integer          not null, primary key
+#  title       :string(250)      not null
+#  description :text
+#  ancestry    :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Parameter < ActiveRecord::Base
   has_ancestry
 
-  attr_accessible :title, :description
+  attr_accessible :title, :description, :parent, :parent_id
 
   validates :title, length: { maximum: 250 }, presence: true
 end
--- spec/models/parameter_spec.rb
+++ spec/models/parameter_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: parameters
+#
+#  id          :integer          not null, primary key
+#  title       :string(250)      not null
+#  description :text
+#  ancestry    :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Parameter do
