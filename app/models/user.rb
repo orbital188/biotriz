695866eb8b55dca1dc452a886a3bdccbeb16185d
nameless <the.nameless.one0@gmail.com>
2013-01-13 13:15:47 +1100

    Refactor `validates` in user.rb (move to the new API)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,15 +16,12 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
 
   attr_accessor :password
-  before_save :prepare_password
+  before_save :prepare_password, :downcase_email
 
-  validates_presence_of :username
-  validates_uniqueness_of :username, :email, :allow_blank => true
-  validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-  validates_presence_of :password, :on => :create
-  validates_confirmation_of :password
-  validates_length_of :password, :minimum => 4, :allow_blank => true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2, maximum: 30 },
+                       format: { with: /^[-\w\._@]+$/i, message: "should only contain letters, numbers, or .-_@" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i }
+  validates :password, presence: { on: :create }, confirmation: true, length: { minimum: 4 }
 
   # login can be either username or email address
   def self.authenticate(login, pass)
@@ -43,5 +40,9 @@ class User < ActiveRecord::Base
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = encrypt_password(password)
     end
+  end
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
